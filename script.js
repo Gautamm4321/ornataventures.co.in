f400e8d616dcc6de5c68cdbbe16d0b4d969af2be
Gautamm4321 <gautam.lakhanii016@gmail.com>
2026-03-05 15:32:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openpage(page) {
+    window.location.href = page;
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
